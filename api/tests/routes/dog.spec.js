380b2726a0234b111cb0f800cc4c8ba90b71519c
yonathanbaez25 <yacevedo170@gmail.com>
2023-07-05 13:30:51 -0400

    Test

--- api/tests/routes/dog.spec.js
+++ api/tests/routes/dog.spec.js
@@ -1,24 +1,33 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
-const session = require('supertest-session');
-const app = require('../../src/app.js');
-const { Dog, conn } = require('../../src/db.js');
+const { expect } = require("chai");
+const session = require("supertest-session");
+const app = require("../../src/app.js");
+const { Dog, conn } = require("../../src/db.js");
 
 const agent = session(app);
 const dog = {
-  name: 'Pug',
+  name: "Pug",
 };
 
-describe('Videogame routes', () => {
-  before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Dog.sync({ force: true })
-    .then(() => Dog.create(dog)));
-  describe('GET /dogs', () => {
-    it('should get 200', () =>
-      agent.get('/dogs').expect(200)
-    );
+describe("Test de RUTAS", () => {
+  describe("GET /dogs/:id", () => {
+    it("Responde con status: 200", async () => {
+      await agent.get("/dogs/1").expect(200);
+    });
+    it('Responde un objeto con las propiedades: "id", "name", "image", "life_span", "temperaments", "minHeight", "maxHeight", "minWeight", "maxWeight"', async () => {
+      const response = (await agent.get("/dogs/1")).body;
+      expect(response).toHaveProperty("id");
+      expect(response).toHaveProperty("name");
+      expect(response).toHaveProperty("image");
+      expect(response).toHaveProperty("life_span");
+      expect(response).toHaveProperty("temperaments");
+      expect(response).toHaveProperty("minHeight");
+      expect(response).toHaveProperty("maxHeight");
+      expect(response).toHaveProperty("minWeight");
+      expect(response).toHaveProperty("maxWeight");
+    });
+    it("Si hay un error responde con status: 500", async () => {
+      await agent.get("/dogs/123456").expect(500);
+    });
   });
 });
